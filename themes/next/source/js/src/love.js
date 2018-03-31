583b3dba50c28da4554f7f3e2271ec1f41016f61
xqmGitHub <xqmmai@gmail.com>
2018-03-31 18:58:31 +0800

    modify config

--- themes/next/source/js/src/love.js
+++ themes/next/source/js/src/love.js
@@ -13,3 +13,27 @@
         }
     });
 })();
+/*document.onreadystatechange = function(){
+    var page = document.getElementById('pageDiv');
+    if (document.readyState == 'interactive') {
+        window.setTimeout(function(){
+            disableLoad();
+        },4000)
+    }
+
+    if (document.readyState == 'complete') {        
+        if (page.classList.contains('js-hidden')) {
+            disableLoad();
+        }
+    }
+}
+
+function disableLoad(){
+    var
+    page = document.getElementById('pageDiv'),
+    loading = document.getElementById('pageLoading');
+
+    loading.classList.add('js-hidden');
+    page.classList.remove('js-hidden');
+	
+}*/
