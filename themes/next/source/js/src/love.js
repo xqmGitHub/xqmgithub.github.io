2ff7c3de3689851eacdaadf7135ba64d183acfb5
xqmGitHub <xqmmai@gmail.com>
2018-09-29 17:18:12 +0800

    modify js

--- themes/next/source/js/src/love.js
+++ themes/next/source/js/src/love.js
@@ -12,6 +12,8 @@
             },1000);
         }
     });
+	console.clear();
+	console.log("%c又特么来偷我代码？", 'background-image:-webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );color:transparent;-webkit-background-clip: text;font-size:2em;');
 })();
 /*document.onreadystatechange = function(){
     var page = document.getElementById('pageDiv');
